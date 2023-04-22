4772e28c69d48f6b8c3fedec24ae4b2a69cdaf0e
yuta-yoshinaga <yuta.yoshinaga@gmail.com>
2023-04-22 20:07:25 +0900

    元に戻す

--- reversiPython/settings.py
+++ reversiPython/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import logging
 
